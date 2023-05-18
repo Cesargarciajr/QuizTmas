62bb727cf64347f3a4c56ab93dd04cf76e2b6778
Cesargarciajr <>
2023-05-18 18:22:11 +0000

    Add style to contact form

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const cards = document.querySelectorAll('.card');
+
+function flipCard() {
+    this.classList.toggle('flip');
+}
+
+
+cards.forEach(card => card.addEventListener('click', flipCard));
