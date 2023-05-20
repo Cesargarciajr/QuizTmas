0b4d7b9681bdfb123becef7e4456b69b260b00c5
Cesargarciajr <>
2023-05-20 11:24:00 +0000

    Add close modal function

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,9 +21,21 @@ openModalCards.forEach(button => {
     });
 });
 
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal');
+        closeModal(modal);
+    });
+});
+
 function openModal(modal) {
     if (modal == null) return;
     modal.classList.add('active');
     overlay.classList.aa('active');
 }
 
+function closeModal(modal) {
+    if (modal == null) return;
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
