eecf394d56e824079bb757e3e8b28402a3bcc990
Cesargarciajr <>
2023-05-20 11:18:32 +0000

    Add open modal function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,24 @@ function flipCard() {
 
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+/* Moldar pop up */
+
+const openModalCards = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+openModalCards.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    });
+});
+
+function openModal(modal) {
+    if (modal == null) return;
+    modal.classList.add('active');
+    overlay.classList.aa('active');
+}
+
