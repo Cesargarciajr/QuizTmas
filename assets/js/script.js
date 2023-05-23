835112c83a0836d9bf9b399d7a4fc2526c349354
Cesargarciajr <>
2023-05-23 01:51:05 +0000

    Add quiz select functions and style

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,7 @@ openModalCards.forEach(button => {
         const modal = document.querySelector(button.dataset.modalTarget);
         openModal(modal);
         startQuestionTimer(modal);
+        startQuiz();
     });
 });
 
@@ -63,4 +64,84 @@ function startQuestionTimer(modal) {
 
 /** Quiz functions */
 
+const questions = [
+    {
+        question: "What year was the first recorded date of Christmas being celebrated?",
+        answers: [
+            { text: "1000 A.D.", correct: false },
+            { text: "1525 B.C.", correct: false },
+            { text: "336 A.D.", correct: true },
+            { text: "1804 C.E", correct: false },
+        ]
+    },
+    {
+        question: "In what country is it believed that the tradition of putting up Christmas trees originated",
+        answers: [
+            { text: "France", correct: false },
+            { text: "Brazil", correct: false },
+            { text: "Ireland", correct: false },
+            { text: "Germany", correct: true },
+        ]
+    },
+];
 
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+const correctAnswered = document.getElementById("correct-answered");
+const incorrectAnswered = document.getElementById("incorrect-answered");
+
+let currentQuestionIndex = 0;
+let score = 0;
+let wrong = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+};
+
+function showQuestion() {
+    resetState();
+    let currenQuestion = questions[currentQuestionIndex];
+    questionElement.innerHTML = currenQuestion.question;
+
+    currenQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+};
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+        correctAnswered.innerHTML = score;
+    } else {
+        selectedBtn.classList.add("incorrect");
+        wrong++;
+        incorrectAnswered.innerHTML = wrong;
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
